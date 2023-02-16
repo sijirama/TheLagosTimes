10e01ca9b01dd3efe142ab597a28b730a877e922
unknown <gbemilesanmi@gmail.com>
2023-02-16 11:30:11 +0100

    axios fetch

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Loading from './components/loader/loader.js'
 function App() {
     
     //NOTE: to eve call or use loading
-    //const [loading ] = useContext(LoadingContext)
+    const [loading ] = useContext(LoadingContext)
 
-    //WARN: this should be removes after testing the loader animation
+    //WARN: this should be removed after testing the loader animation
     //NOTE: this can be used to set loader off and on in every other component
-    const [loading, setLoading] = useContext(LoadingContext)
-    setLoading(true)
+    //const [loading, setLoading] = useContext(LoadingContext)
+    //setLoading(true)
 
     return (
       !loading? (<div className="app">
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import Header from '../../components/header/Header.js'
-    
+import { LoadingContext } from '../../context/loading.js'
+import {API_KEY} from '../../api/index.js'
+import {NEWS_URL} from '../../api/index.js'
+import axios from 'axios'    
 
 function Home() {
-  return (
-    <div className="home-wrapper">
-        <Header />
-    </div>
-  )
+    const [newsdata, setNewsdata] = useState([])
+    const [error, setError] = useState([])
+    const [loading, setLoading] = useContext(LoadingContext)
+    const newsurl = `${NEWS_URL}apikey=${API_KEY}`
+    const fakeapi = "https://jsonplaceholder.typicode.com/posts"
+
+    useEffect(() => {
+            setLoading(true)    
+
+            axios.get(`${fakeapi}`)
+                .then((response) => {setNewsdata(response.data)})
+                .catch(error => {setError(error)})
+
+            setLoading(false)
+    },[])
+
+    
+    console.log(newsdata)
+
+    return (
+        <div className="home-wrapper">
+            <Header />
+        </div>
+    )
 }
 
 export default Home

4476ebe971689cd2e1a31247cd67b407d75d269d
unknown <gbemilesanmi@gmail.com>
2023-02-17 17:53:16 +0100

    datetime

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,7 +3,16 @@ import "./navbar.scss"
 
 function Navbar() {
 
-  const date = "Wednesday 16, Febrary 2023"
+  //const date = "Wednesday 16, Febrary 2023"
+  const date = getCurrentDate() 
+
+  function getCurrentDate(separator=' '){
+        let newDate = new Date()
+        let date = newDate.getDate();
+        let month = newDate.getMonth() + 1;
+        let year = newDate.getFullYear();
+        return `${year}${separator}${month<10?`0${month}`:`${month}`}${separator}${date}`
+        }
 
   return (
     <div className="nav">
